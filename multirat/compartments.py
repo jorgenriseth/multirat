66ce4f337abdef76c0beffbe6cec9e37bc62abf7
Jørgen Riseth <jnriseth@gmail.com>
2022-10-24 12:11:01 +0200

    removed unnecessary files

--- multirat/compartments.py
+++
@@ -1,7 +0,0 @@
-class Compartment:
-    def __init__(self, idx, name, porosity, permeability,
-                viscosity):
-        self.idx = idx
-        self.name = name
-        self.porosity = porosity
-
